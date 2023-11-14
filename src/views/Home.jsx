38b63d0cb6279d7c54ad19f1924e34696a56d471
AlbanMarku <albanmarku@outlook.com>
2023-11-14 16:49:48 +0000

    first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import NavComp from '../components/NavComp';
+import '../styles/home.css';
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <NavComp />
+    </div>
+  );
+}
