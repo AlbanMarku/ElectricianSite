72e94c6563126189467dd23c61625d39a567fb2d
AlbanMarku <albanmarku@outlook.com>
2023-11-18 13:58:30 +0000

    basic layout review.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -6,11 +6,19 @@ export default function Review() {
   return (
     <div className="Review">
       <Container>
-        <div className="review-img" />
-        <div className="review-info">
-          <div className="stars">star</div>
-          <div className="review-text">
-            <p>review here</p>
+        <div className="container">
+          <div className="review-img" />
+          <div className="review-info">
+            <div className="stars">star</div>
+            <div className="review-text">
+              <p>
+                &quot; Rapid response from Luca. We couldn&apos;t work out why
+                the power to kitchen was tripping, we called Luca quite late
+                around 5pm after another electrician let us down, and he was
+                able to attend same day! Issue was traced and resolved in quick
+                order, very happy. &quot;
+              </p>
+            </div>
           </div>
         </div>
       </Container>
