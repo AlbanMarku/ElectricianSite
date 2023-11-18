9c0eade3323b3c6d2ffe73c9a0241a9d0c77c1e7
AlbanMarku <albanmarku@outlook.com>
2023-11-18 15:27:39 +0000

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './views/Home';
+import Services from './views/Services';
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/NavComp.jsx
+++ src/components/NavComp.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 import '../styles/navComp.css';
 
@@ -12,27 +12,16 @@ export default function NavComp() {
         <Container fluid style={{ maxWidth: '1500px' }}>
           <Navbar expand="lg" className="nav-bar">
             <Container>
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Brand href="#home">The One Electrician</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
-                      Action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                      Separated link
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                <Nav className="nav-links me-auto">
+                  <Link className="sub-link" to="/">
+                    Home
+                  </Link>
+                  <Link className="sub-link" to="/services">
+                    Our Services
+                  </Link>
                 </Nav>
               </Navbar.Collapse>
             </Container>
