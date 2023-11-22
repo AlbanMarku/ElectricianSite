a1171ae589500c0ff0072557bbf6c6062fb59514
AlbanMarku <albanmarku@outlook.com>
2023-11-22 14:43:39 +0000

    Services styled fully.

--- src/components/ServicesComp.jsx
+++ src/components/ServicesComp.jsx
@@ -9,11 +9,35 @@ export default function ServicesComp() {
         <div className="service-img" />
 
         <div className="service-info">
+          <h1>Our Services</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatibus laborum aperiam cumque porro cupiditate, culpa
-            explicabo nostrum placeat perferendis ad magnam in! Sunt quos
-            dignissimos quae, consectetur reprehenderit voluptatum eveniet?
+            Here at The One Electrician Ltd, we provide a complete range of high
+            quality, professional electrical services in London and the
+            surrounding areas, covering all aspects of electrical including the
+            following:
+          </p>
+          <ul>
+            <li>EICR Safety Checks</li>
+            <li>Full re-wiring</li>
+            <li>Part re-wiring</li>
+            <li>Periodic Inspection and Testing</li>
+            <li>New Installations/Partial Installations</li>
+            <li>Fuseboards</li>
+            <li>PAT Testing</li>
+            <li>Fault Finding</li>
+            <li>Smoke Alarms</li>
+            <li>Earth Bonding</li>
+            <li>Cooker Installation</li>
+            <li>Electric Shower</li>
+            <li>Bathroom Radiators</li>
+            <li>Commercial and Domestic Jobs</li>
+          </ul>
+          <p>
+            So if you&apos;re looking for a electrical company in London with
+            many years experience in the electrical trade then look no further
+            than The One Electrician Ltd. Whether it&apos;s a query or a quote,
+            pick up the phone and call 07590360176 today or visit our contact
+            page or send us an email.
           </p>
         </div>
       </Container>
