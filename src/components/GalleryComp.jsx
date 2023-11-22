936a4eaa28ec2e6870984d1b13bacec1c7bf3599
AlbanMarku <albanmarku@outlook.com>
2023-11-22 15:36:49 +0000

    Basic lightbox.

--- src/components/GalleryComp.jsx
+++ src/components/GalleryComp.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import Lightbox from 'yet-another-react-lightbox';
+// eslint-disable-next-line import/no-unresolved
+import 'yet-another-react-lightbox/styles.css';
+
+import img2 from '../assets/images/review.jpg';
+import img1 from '../assets/images/service.jpg';
 
 export default function GalleryComp() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="GalleryComp">
-      <Container>
+      <Container style={{ maxWidth: '1500px' }}>
         <h1>Some content</h1>
+        <button type="button" onClick={() => setOpen(!open)}>
+          open me
+        </button>
+        <Lightbox
+          open={open}
+          close={() => setOpen(false)}
+          slides={[{ src: img1 }, { src: img2 }]}
+        />
       </Container>
     </div>
   );
