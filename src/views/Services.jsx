30455d7af5c5f8b99612e0d1b17f01ba5a9c9115
AlbanMarku <albanmarku@outlook.com>
2023-11-26 14:47:57 +0000

    Info comp.

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -2,11 +2,11 @@ import Banner from '../components/Banner';
 import ChooseUs from '../components/ChooseUs';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
+import InfoComp from '../components/InfoComp';
 import Main from '../components/Main';
 import NavComp from '../components/NavComp';
 import Review from '../components/Review';
 import ServicesComp from '../components/ServicesComp';
-import WhyUsComp from '../components/WhyUsComp';
 
 export default function Services() {
   const heroContent = 'Providing high quality electrical, services in London';
@@ -16,7 +16,7 @@ export default function Services() {
       <Banner />
       <Hero content={heroContent} size="sm" />
       <Main Comp={<ServicesComp />} />
-      <ChooseUs WhyUs={<WhyUsComp />} />
+      <ChooseUs WhyUs={<InfoComp />} />
       <Review />
       <Footer />
     </div>
