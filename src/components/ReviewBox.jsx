7d2f4abd7abde1aba517ba85c874d4dbd0d13cf3
AlbanMarku <albanmarku@outlook.com>
2023-11-29 14:47:43 +0000

    reviews

--- src/components/ReviewBox.jsx
+++ src/components/ReviewBox.jsx
@@ -1,11 +1,20 @@
 import { Rating } from '@mui/material';
+import { FaLocationDot } from 'react-icons/fa6';
 
 import '../styles/reviewBox.css';
 
-export default function ReviewBox() {
+export default function ReviewBox({ location, review }) {
   return (
     <div className="ReviewBox">
       <Rating readOnly value="5" size="large" />
+      <p>{`"${review}"`}</p>
+      <div className="details">
+        <h4>Customer from Checkatrader</h4>
+        <div className="details-name">
+          <FaLocationDot style={{ marginTop: '-5px' }} />
+          <p>{location}</p>
+        </div>
+      </div>
     </div>
   );
 }
