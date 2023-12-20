19ee58d52d54c3e221dd4894b6225173c177dde7
AlbanMarku <albanmarku@outlook.com>
2023-12-20 15:10:18 +0000

    Reviews logic.

--- src/components/TestimonialsComp.jsx
+++ src/components/TestimonialsComp.jsx
@@ -1,5 +1,7 @@
 import Container from 'react-bootstrap/Container';
+import { v4 as uuidv4 } from 'uuid';
 
+import reviewData from '../reviews.json';
 import ReviewBox from './ReviewBox';
 
 export default function TestimonialsComp() {
@@ -8,18 +10,13 @@ export default function TestimonialsComp() {
       <Container style={{ maxWidth: '1500px' }}>
         <h1>Testimonials</h1>
         <main>
-          <ReviewBox
-            location="London"
-            review=" Luca came to do an EICR and did a great job. There were a few extra bit of work identified as needing doing which his electrician came to do the following week. Work was done quickly and to a great standard. We were pleased. "
-          />
-          <ReviewBox
-            location="Hounslow"
-            review=" Really good and reliable , friendly and very professional. Would recommended and will definitely use again. "
-          />
-          <ReviewBox
-            location="London"
-            review=" Luca and Mirjan are a great team. They are both very professional. Luca gave a good price for the work, and Mirjan carried it out very efficiently. Would recommend the One! "
-          />
+          {reviewData.map((item, index) => (
+            <ReviewBox
+              location={item.location}
+              review={item.review}
+              key={uuidv4()}
+            />
+          ))}
         </main>
       </Container>
     </div>
